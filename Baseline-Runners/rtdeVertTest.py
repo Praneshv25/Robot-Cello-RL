d760bf37ea5d5d3478cb7341856f5d3c853a8c3a
Rohan Pradhan <79280764+RR311@users.noreply.github.com>
2026-03-05 18:12:57 -0500

    Add files via upload
    This file contains the framework that makes servoL, servoJ, and the force function work.

--- Baseline-Runners/rtdeVertTest.py
+++ Baseline-Runners/rtdeVertTest.py
@@ -17,18 +17,27 @@ rtde_r = rtde_receive.RTDEReceiveInterface(ROBOT_IP)
 try:
 
     #actual_q = rtde_r.getActualQ()
-    current_pos = rtde_r.getActualTCPPose()
-    print(current_pos)
+    #current_pos = rtde_r.getActualTCPPose()
+    #print(current_pos)
 
-    up_pose = list(current_pos)
-    up_pose[2] += .10
-    print("Moving up 10cm")
+    #up_pose = list(current_pos)
+    #up_pose[2] += .10
+    #print("Moving up 10cm")
 
     #rtde_c.moveL(up_pose, speed = 0.1, acceleration = 0.2)
 
     tcp_pose = rtde_r.getActualTCPPose()
-    tcp_pose[2] += .20
+
+    #print(tcp_pose)
+    #print(rtde_c.getInverseKinematics(tcp_pose))
+
+    #tcp_pose[1] += .20
+    
+    #print(tcp_pose)
     j_pos = rtde_c.getInverseKinematics(tcp_pose)
+    #print(j_pos)
+
+    #j_pos[2] += .2
     #rtde_c.servoJ(j_pos, time = 0.1, lookahead_time = 0.05, gain = 300.0)
     """
     for i in range(len(tcp_pose)):
@@ -40,12 +49,37 @@ try:
 
     #rtde_c.servoL(tcp_pose, .25 ,1.2 , 1, 0.1, 1000.0)
 
-    rtde_c.servoJ(j_pos, time = 0.1, lookahead_time = 0.05, gain = 300.0)
+    #rtde_c.servoJ(j_pos, time = 0.1, lookahead_time = 0.05, gain = 300.0)
+    
+    move_speed = [0.0, 0.2 ,0, 0.0, 0.0, 0.0]
+    accel = 0.25
+    #rtde_c.speedL(move_speed, acceleration = accel, time=2)
+    print(rtde_r.getActualTCPForce()[3])
 
+    for i in range(500):
+        rtde_c.forceMode(rtde_r.getActualTCPPose(), [0,0,1,0,0,0], [0,0,5,0,0,0], 2, [2,2,1.5,1,1,1])
+        t_start = rtde_c.initPeriod()
+        #rtde_c.speedL(move_speed, acceleration = accel, time=2)
+        #rtde_c.servoJ(j_pos, 0.5, 0.5, 1/500 , 0.1, 500)
+        rtde_c.servoL(tcp_pose, 0, 0, 1/500 , 0.1, 500)
+        tcp_pose[1] -= .0009
+        #tcp_pose = rtde_r.getActualTCPPose()
+        
+
+        j_pos = rtde_c.getInverseKinematics(tcp_pose)
+        rtde_c.servoJ(j_pos, 0, 0, 1/500 , 0.1, 500)
+        #rtde_c.servoL(tcp_pose, 0, 0, 1/500 , 0.1, 500)
+        print(rtde_r.getActualTCPForce()[3])
+        
+
+        rtde_c.waitPeriod(t_start)
+        
+        #rtde_c.servoJ(j_pos, speed = 0, acceleration = 0, time =1/500, lookahead_time=0.1, gain=500)
+
+    rtde_c.servoStop()
     print("movement complete")
 
 except Exception as e:
     print(f"An error has occured: {e}")
 finally:
     rtde_c.stopScript()
-
